0dd452f9c3107c84a7a7f43232b9d281bad93ed5
Mher Kazandjian <mherkazandjian@gmail.com>
2017-05-02 21:26:18 +0200

    add interface methods for the datasource base class

--- pytest_ds/data_sources/interface.py
+++ pytest_ds/data_sources/interface.py
@@ -1,4 +1,8 @@
 class DataSourceBase(object):
-    def __init__(self):
-        print('DatasourceBase init')
-        pass
+    def __init__(self, url, *args, **kwargs):
+        self._url = url
+    def ls(self):
+        raise NotImplementedError('should be implemented by subclass')
+    def ls_url(self):
+        raise NotImplementedError('should be implemented by subclass')
+
