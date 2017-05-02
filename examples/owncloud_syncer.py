c89d1c034b65a4daaec44261f9332bd0abddce0f
Mher Kazandjian <mherkazandjian@gmail.com>
2017-05-02 21:28:06 +0200

    add example script

---
+++ examples/owncloud_syncer.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import os
+
+from pytest_ds.tree import Query
+
+config_file = os.path.expanduser('~/tmp/owncloud_pytest_ds_webdav.ini')
+# config_file = os.path.expanduser('~/tmp/surfdrive.ini')
+
+tree = Query(config=config_file, index_webdav_enabled=True)
+
+# tree.show()
+# tree.ls()
+# tree.ls_url()
+
+# tree.sync()
+
+print('done')
