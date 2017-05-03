f4d07d36527785d281e9294a67000b24ff9034bb
Mher Kazandjian <mherkazandjian@gmail.com>
2017-05-03 15:13:16 +0200

    update an interface  method docstring

--- pytest_ds/data_sources/interface.py
+++ pytest_ds/data_sources/interface.py
@@ -1,8 +1,30 @@
 class DataSourceBase(object):
+    """
+    
+    """
     def __init__(self, url, *args, **kwargs):
+        """
+        
+        :param url: 
+        :param args: 
+        :param kwargs: 
+        """
         self._url = url
+
     def ls(self):
+        """
+        
+        :return: 
+        """
         raise NotImplementedError('should be implemented by subclass')
+
     def ls_url(self):
+        """should return a dictionary. The keys of the dictionary are the
+        paths on the file as they would be accessed on a local file system
+        .e.g path/to/file1 (not there should be no / at the beginning). The
+        value correcsponding to each key is a [Content object, download url]
+        
+        :return: 
+        """
         raise NotImplementedError('should be implemented by subclass')
 
