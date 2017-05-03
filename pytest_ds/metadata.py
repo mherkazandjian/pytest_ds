0d784d26943deb202013f0d02bb826cb82cde2cd
Mher Kazandjian <mherkazandjian@gmail.com>
2017-05-03 12:20:43 +0200

    add the metadata file

---
+++ pytest_ds/metadata.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'pytest_ds'
+project = "pytest plugin for external test data sources."
+project_no_spaces = project.replace(' ', '')
+version = '0.1.0'
+description = '.. todo:: long description here.'
+authors = ['Mher Kazandjian']
+authors_string = ', '.join(authors)
+emails = ['mherkazandjian@gmail.com']
+license = 'GLP-3.0'
+copyright = '2017 ' + authors_string
+url = 'https://github.com/mherkazandjian/pytest_ds'
